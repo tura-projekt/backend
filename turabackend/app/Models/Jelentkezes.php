bd634ba851219aa3c75b8c2ff219fedc50fc7c38
zltnszekeres <zltn.szekeres@yahoo.com>
2024-02-23 16:56:09 +0100

    jelenkezes modell osszetett kulcs

--- turabackend/app/Models/Jelentkezes.php
+++ turabackend/app/Models/Jelentkezes.php
@@ -7,5 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jelentkezes extends Model
 {
-    use HasFactory;
+    use HasFactory;protected function setKeysForSaveQuery($query)
+    {
+        $query
+            ->where('user_id', '=', $this->getAttribute('user_id'))
+            ->where('tura_id', '=', $this->getAttribute('tura_id'));
+            
+        return $query;
+    }  protected $fillable = [
+        'user_id',
+        'tura_id',
+        'jelentkezes_datuma',
+        'fizetve'
+        
+    ];
+
+
+
+
+
+
+
 }
